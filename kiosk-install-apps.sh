83b950b790ade86938db61c3b037bd1c77485823
Emmanuel Geoffray <emmanuel@soixantecircuits.fr>
2015-04-12 12:44:12 +0200

    Comments heavy php stuff

--- kiosk-install-apps.sh
+++ kiosk-install-apps.sh
@@ -26,6 +26,7 @@ cd
 sudo apt-get install autoconf --yes
 sudo apt-get install libtool --yes
 sudo apt-get install g++ make --yes
+
 # install python stuff
 # -----------------
 sudo apt-get install python-software-properties python python-pip --yes
@@ -39,14 +40,14 @@ sudo apt-get install gdevilspie --yes
 
 # install lamp stuff
 # -----------------
-sudo apt-get install apache2   --yes
-sudo apt-get install -y mysql-server mysql-client 
-sudo apt-get install -y php5 libapache2-mod-php5 
-sudo /etc/init.d/apache2 restart
-sudo apt-get search php5
-sudo apt-get install -y php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-json 
-/etc/init.d/apache2 restart
-sudo apt-get install -y phpmyadmin 
+#sudo apt-get install apache2   --yes
+#sudo apt-get install -y mysql-server mysql-client 
+#sudo apt-get install -y php5 libapache2-mod-php5 
+#sudo /etc/init.d/apache2 restart
+#sudo apt-get search php5
+#sudo apt-get install -y php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-json 
+#/etc/init.d/apache2 restart
+#sudo apt-get install -y phpmyadmin 
  
 # install nodejs stuff
 # -----------------
@@ -55,13 +56,17 @@ sudo apt-get update -y
 sudo apt-get install -y nodejs npm
 sudo apt-get install -y nodejs-legacy
 
+# install image stuff
+# -----------------
+sudo apt-get install -y image-magick
+
 
 # install teamviewer
 # -----------------
 # TODO: check still working on ubuntu gnome
-sudo wget http://download.teamviewer.com/download/version_8x/teamviewer_linux_x64.deb
-sudo dpkg -i teamviewer_linux_x64.deb
-sudo apt-get install -f -yes
+#sudo wget http://download.teamviewer.com/download/version_8x/teamviewer_linux_x64.deb
+#sudo dpkg -i teamviewer_linux_x64.deb
+#sudo apt-get install -f -yes
 
 
 # install dropbox 
