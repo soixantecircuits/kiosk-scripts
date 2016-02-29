8f17b61741baf607e7357ead398b7c761cbe9fa2
mina <mina@carpe>
2016-02-29 17:04:18 +0100

    updated

--- kiosk-sm/kiosk-sm.sh
+++ kiosk-sm/kiosk-sm.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
 gnome-terminal --tab -e "/usr/bin/mono '/usr/share/ssm/SM.App.Linux.exe'"
-sleep 2
-gnome-terminal --tab -e "/usr/bin/devilspie"
-sleep 1
-xdotool mousemove 449 136 click 1
+while true; do
+  if ps aux | grep -v grep | grep "/usr/share/ssm/SM.App.Linux.exe"
+  then
+    echo "found"
+    sleep 2
+    gnome-terminal --tab -e "/usr/bin/devilspie"
+    sleep 1
+    xdotool mousemove 375 145 click 1
+    sleep 3
+    break
+  else
+    echo "run"
+  fi
+done
